a02ebc76f8960f73efd326bb4c2a2624b4aa32c4
Ayushman013-amp <ayushmanpati5@gmail.com>
2026-09-25 21:11:03 +0530

    Fix hero text vertical alignment to reduce top gap

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <section className="bg-white pt-0 lg:pt-2 pb-24 overflow-hidden">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex flex-col lg:flex-row items-center gap-16">
+        <div className="flex flex-col lg:flex-row lg:items-start lg:pt-12 items-center gap-16">
 
           <div className="w-full lg:w-1/2 flex flex-col items-start text-left">
 
